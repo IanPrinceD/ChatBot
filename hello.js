8802f57bf7bc67dd377011e89aa9e315105b1737
IanPrinceD <princedumanhug@gmail.com>
2023-09-01 14:49:38 +0800

    Uploading the whole file

---
+++ hello.js
@@ -0,0 +1,28 @@
+function chat() {
+  alert('Click the START button!');
+
+  const yourName = prompt('Type your name');
+  
+
+  if (yourName === null) {
+    return;
+  }
+
+  alert('Hi, ' + yourName);
+
+  const mood = prompt('Are you good?');
+  
+  if (mood === null) {
+    return; 
+  }
+
+  if (mood === "Yes") {
+    alert('Great! You are ' + mood + ' today');
+  }
+  else if (mood === "No") {
+    alert('Sorry to hear that.');
+  }
+  else {
+    alert('Cool!');
+  }
+}
